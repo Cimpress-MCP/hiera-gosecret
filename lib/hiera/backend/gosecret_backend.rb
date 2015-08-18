065f09e154f73ca27fb77c7c7b226540dc47f0a0
Calvin Leung Huang <cleung2010@gmail.com>
2015-08-18 17:10:12 -0400

    Remove unused 'which' method

--- lib/hiera/backend/gosecret_backend.rb
+++ lib/hiera/backend/gosecret_backend.rb
@@ -60,17 +60,6 @@ class Hiera
           return `gosecret #{args}`
         end
       end
-
-      def which(cmd)
-        exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
-        ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
-          exts.each { |ext|
-            exe = File.join(path, "#{cmd}#{ext}")
-            return exe if File.executable?(exe) && !File.directory?(exe)
-          }
-        end
-        return nil
-      end
     end
   end
 end
